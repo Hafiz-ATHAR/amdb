4fd0293f51c8eda6fc60c9c2e0dfd012aa691f0b
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-11-01 06:47:42 -0700

    Initialize Project

---
+++ src/main/java/com/AMDb/moviedatabase/MoviedatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.AMDb.moviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedatabaseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/AMDb/moviedatabase/MoviedatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.AMDb.moviedatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviedatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
