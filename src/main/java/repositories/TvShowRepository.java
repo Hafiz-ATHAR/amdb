1fa84d7d461e1564f6eeb27501994a2c45d8ff7d
Hafiz-ATHAR <h.m.athar1@hotmail.com>
2018-12-05 14:23:11 -0800

    Create TvShow repository

---
+++ src/main/java/repositories/TvShowRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.amdb.moviedatabase.model.TvShow;
+
+public interface TvShowRepository extends CrudRepository<TvShow, Long>{
+
+}
